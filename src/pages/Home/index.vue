77977cb5c71c19c61d611334f3bc9ffc4f377a2f
Hellopbs <153763699@qq.com>
2022-10-25 23:33:53 +0800

    1111

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,21 +1,38 @@
 <template>
-  <TypeNav/>
+  <div>
+    <TypeNav />
+    <ListContainer />
+    <Recommend></Recommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Brand></Brand>
+  </div>
+
 </template>
 
 <script>
-import TypeNav from './TypeNav/index.vue';
+import ListContainer from '@/pages/Home/ListContainer'
+import Recommend from '@/pages/Home/Recommend'
+import Rank from '@/pages/Home/Rank'
+import Like from '@/pages/Home/Like'
+import Floor from '@/pages/Home/Floor'
+import Brand from '@/pages/Home/Brand'
 export default {
   data() {
     return {
     };
   },
-  components: { TypeNav },
+  components: {
+    ListContainer,Recommend,Rank,Like,Floor,Brand
+  },
   props: {},
   computed: {},
   methods: {},
-  created() {},
-  mounted() {}
+  created() { },
+  mounted() { }
 };
 </script>
 <style>
+
 </style>
